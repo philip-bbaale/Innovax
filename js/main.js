bbe3bc67f0288de7adebec84e1d93eb625734fad
Pip Bbaale <namugera.philip@gmail.com>
2020-10-24 12:34:31 +0300

    redesigning home page'

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-document.getElementById("logo").children[0].className += "load";
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".panel:not(:last-child)", {
+//     yPercent: -100, 
+//     ease: "none",
+//     stagger: 0.5,
+//     scrollTrigger: {
+//       trigger: "#container",
+//       start: "top top",
+//       end: "+=300%",
+//       scrub: true,
+//       pin: true
+//     }
+//   });
+  
+  
+//   gsap.set(".panel", {zIndex: (i, target, targets) => targets.length -i});
+  
+  
