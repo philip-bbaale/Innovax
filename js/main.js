fd77f7b541366a4d1e941fdb319e5c214a384457
Pip Bbaale <namugera.philip@gmail.com>
2020-10-28 07:46:32 +0300

    adding animation to home text

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".panel:not(:last-child)", {
-//     yPercent: -100, 
-//     ease: "none",
-//     stagger: 0.5,
-//     scrollTrigger: {
-//       trigger: "#container",
-//       start: "top top",
-//       end: "+=300%",
-//       scrub: true,
-//       pin: true
-//     }
-//   });
-  
-  
-//   gsap.set(".panel", {zIndex: (i, target, targets) => targets.length -i});
+gsap.from(".inovative_text", {duration: 1, opacity: 0, y: 150, stagger:0.25}, "+=1");
+
   
   
